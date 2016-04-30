fe49ca0c9b570ba6ed46523d31382d87eebc3891
Ekta Sorathia <ektasorathia@gmail.com>
2016-04-30 03:39:23 -0700

    farmer RMQ updated

--- services/farmer.js
+++ services/farmer.js
@@ -94,3 +94,73 @@ exports.doUpdateProfile = function(msg, callback)
     }
   });
 }
+
+exports.doAddIntroduction = function(msg, callback)
+{
+  console.log("Product JSON ");
+  
+  var updatedWhereJSON = msg.WhereJSON;
+  var updatedDetailJSON = msg.introductionSetJSON
+  
+  mongo.updateOne('FARMER_DETAILS',updatedWhereJSON,updatedDetailJSON,function(err,searchRes){
+
+    if(err){
+      throw err;
+    }
+    else
+    {
+      console.log("Result received in doUpdateProfile");
+      if(searchRes){
+        var jsonResponse = {
+          "searchResults" : searchRes,
+          "statusCode" : 200
+        };
+        callback(null, jsonResponse);
+      }
+      else {
+        jsonResponse = {result : "Nothing Found", "status" : "OK"};
+        callback(null, jsonResponse);
+      }
+    }
+  });
+}
+
+exports.doDeleteProfile = function(msg, callback)
+{
+  console.log("Product JSON ");
+  
+  var updatedWhereJSON = msg.deleteWhereJSON;
+  var updatedDetailJSON = msg.deleteSetJSON
+  
+  mongo.updateOne('FARMER_DETAILS',updatedWhereJSON,updatedDetailJSON,function(err,searchRes){
+
+    if(err){
+      throw err;
+    }
+    else
+    {
+      if(searchRes){
+          console.log("Result received in doDeleteProfile");
+          var deleteQuery = "UPDATE USERS SET IS_APPROVED = " + 0 +" WHERE USER_ID = "+ updatedWhereJSON.USER_ID;
+          mysql.updateData(deleteQuery,function (err, results) {
+            if(err){
+              throw err;
+            }else{
+                var jsonResponse = {
+                "searchResults" : searchRes,
+                "statusCode" : 200
+                };
+                callback(null, jsonResponse);
+              }
+          });
+        }
+      else {
+      
+          jsonResponse = {result : "Nothing Found", "status" : "OK"};
+          callback(null, jsonResponse);
+        }
+      
+    }   
+  });
+}
+
