acddd8b4c716acaf3a596276998faf63d5e70ac4
Ekta Sorathia <ektasorathia@gmail.com>
2016-04-30 23:38:13 -0700

    Farmer queue updated

--- services/farmer.js
+++ services/farmer.js
@@ -72,7 +72,7 @@ exports.doUpdateProfile = function(msg, callback)
   var updatedWhereJSON = msg.updatedWhereJSON;
   var updatedDetailJSON = msg.updatedDetailJSON
   
-  mongo.updateOne('USERS',updatedWhereJSON,updatedDetailJSON,function(err,searchRes){
+  mongo.updateOne('USER_DETAILS',updatedWhereJSON,updatedDetailJSON,function(err,searchRes){
 
     if(err){
       throw err;
@@ -132,7 +132,7 @@ exports.doDeleteProfile = function(msg, callback)
   var updatedWhereJSON = msg.deleteWhereJSON;
   var updatedDetailJSON = msg.deleteSetJSON
   
-  mongo.updateOne('FARMER_DETAILS',updatedWhereJSON,updatedDetailJSON,function(err,searchRes){
+  mongo.updateOne('USER_DETAILS',updatedWhereJSON,updatedDetailJSON,function(err,searchRes){
 
     if(err){
       throw err;
@@ -161,6 +161,46 @@ exports.doDeleteProfile = function(msg, callback)
         }
       
     }   
+  });
+}
+
+exports.addFarmerReview = function(msg, callback)
+{
+  console.log("Product JSON ");
+  
+  var productWhereJSON = msg.productWhereJSON;
+  var productSetJSON = msg.productSetJSON
+  var updateAvgRating = msg.updateAvgRating;
+  mongo.updateOne('FARMER_DETAILS',productWhereJSON,productSetJSON,function(err,searchRes){
+
+    if(err){
+      throw err;
+    }
+    else
+    {
+      console.log("Result received in addFarmerReview");
+      if(searchRes){
+          mongo.updateOne('PRODUCTS',productWhereJSON,updateAvgRating,function(err,reviews){
+          if(err){
+            throw err;
+            json_responses = {"statusCode" : 401};
+            console.log("Error in doShowProductList");
+            res.send(json_responses);
+          }
+        else{
+            var jsonResponse = {
+                                "searchResults" : reviews,
+                                "statusCode" : 200
+                        };
+            callback(null, jsonResponse);
+        }
+      });
+    }
+    else {
+        jsonResponse = {result : "Nothing Found", "status" : "OK"};
+        callback(null, jsonResponse);
+      }
+    }
   });
 }
 
